ac8288d6fb0017dfcea088f15c530792bcc1cc66
Stanislav Baglaev <stasbaglaev@yandex.ru>
2021-08-27 12:25:41 +0300

    Added a complete solution to the task mail

---
+++ src/stepik/basiccourse/mail/service/UntrustworthyMailWorker.java
@@ -0,0 +1,25 @@
+package stepik.basiccourse.mail.service;
+
+import stepik.basiccourse.mail.sendable.Sendable;
+
+public class UntrustworthyMailWorker implements MailService {
+
+    private static final RealMailService realMailService = new RealMailService();
+    private final MailService[] otherServices;
+
+    public UntrustworthyMailWorker(final MailService[] otherServices) {
+        this.otherServices = otherServices;
+    }
+
+    public RealMailService getRealMailService() {
+        return realMailService;
+    }
+
+    @Override
+    public Sendable processMail(Sendable mail) {
+        for (MailService service : otherServices) {
+            service.processMail(mail);
+        }
+        return realMailService.processMail(mail);
+    }
+}
