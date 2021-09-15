efd7866678709e3f622dde2fe54d51a88716d6a6
Stanislav Baglaev <stasbaglaev@yandex.ru>
2021-09-15 12:06:47 +0300

    Added solution for block 3.4

---
+++ all/src/stepik/basiccourse/all/inheritance34/ComplexNum.java
@@ -0,0 +1,18 @@
+package stepik.basiccourse.all.inheritance34;
+
+public class ComplexNum extends ComplexNumber {
+
+    public ComplexNum(double re, double im) {
+        super(re, im);
+    }
+
+    @Override
+    double getRe() {
+        return super.getRe();
+    }
+
+    @Override
+    public double getIm() {
+        return super.getIm();
+    }
+}
