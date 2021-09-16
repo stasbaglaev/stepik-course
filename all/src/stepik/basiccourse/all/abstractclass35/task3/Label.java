f7bc28ef32fa4fca94fac62597d06cbb01e2c86d
Stanislav Baglaev <stasbaglaev@yandex.ru>
2021-09-16 17:32:25 +0300

    Added solution for block 3.5 task3

---
+++ all/src/stepik/basiccourse/all/abstractclass35/task3/Label.java
@@ -0,0 +1,5 @@
+package stepik.basiccourse.all.abstractclass35.task3;
+
+enum Label {
+    SPAM, NEGATIVE_TEXT, TOO_LONG, OK
+}
---
+++ all/src/stepik/basiccourse/all/abstractclass35/task3/TextAnalyzer.java
@@ -0,0 +1,5 @@
+package stepik.basiccourse.all.abstractclass35.task3;
+
+interface TextAnalyzer {
+    Label processText(String text);
+}
