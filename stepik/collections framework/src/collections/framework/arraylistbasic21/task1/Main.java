ad9379f842bef22e2f6fd7e921f69c72a50e7346
Stanislav Baglaev <stasbaglaev@yandex.ru>
2021-10-05 18:03:23 +0300

    Added solution for block 2.1 task1 (collections framework)

---
+++ stepik/collections framework/src/collections/framework/arraylistbasic21/task1/Main.java
@@ -0,0 +1,15 @@
+package collections.framework.arraylistbasic21.task1;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection<?> collection;
+    }
+
+    public static Integer maxElem(List<Integer> list){
+        return Collections.max(list);
+    }
+}
