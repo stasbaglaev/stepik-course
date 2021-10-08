80a13914ec2183bd02cf977dd61d4574a40cc4ac
Stanislav Baglaev <stasbaglaev@yandex.ru>
2021-10-08 13:38:53 +0300

    Added solution for block 2.12 task1 (collections framework)

---
+++ stepik/collections framework/src/collections/framework/iterator212/Main.java
@@ -0,0 +1,27 @@
+package collections.framework.iterator212;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = reader.readLine().split(" ");
+        LinkedList<Integer> list = new LinkedList<>();
+        for (String string : str) {
+            list.add(Integer.valueOf(string));
+        }
+        Collections.sort(list);
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() % 2 == 0) {
+                iterator.remove();
+            }
+        }
+        list.forEach(System.out::println);
+    }
+}
